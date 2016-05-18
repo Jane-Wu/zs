18c7d15946b9a88e223aa692ac6ae0010efcf76b
Lang <lang.wu.jane@gmail.com>
2016-05-18 23:25:20 +0800

    Refine google search result

--- modules/custom/capital_news/src/Controller/NewsController.php
+++ modules/custom/capital_news/src/Controller/NewsController.php
@@ -9,40 +9,22 @@ namespace Drupal\capital_news\Controller;
 use Drupal\Core\Controller\ControllerBase;
 
 class NewsController extends ControllerBase {
-  const GOOGLERESULT = 'Google';
 
   public function content() {
     $config = \Drupal::config('capital_news.settings');
-    //if($config->get('search.result') == null || $config->get('search.cache_keys') != $config->get('search.search_keys')){
-    if($config->get('search.result') == null ){
-
-      \Drupal::logger('capital-test')->notice(print_r('live search', true));
-
-      \Drupal::logger('capital-test')->notice(print_r($config->get('search.search_keys'), true));
-      $keys = explode('，', $config->get('search.search_keys'));
-      $news = array();
-      $news_ra = array();
-      foreach ( $keys as $key){
-        $this->getGoogleNews($news, $news_ra, trim($key));
-      }
-      \Drupal::service('config.factory')->getEditable('capital_news.settings')->set('search.cache_keys', $config->get('search.search_keys'))->save();
-      \Drupal::service('config.factory')->getEditable('capital_news.settings')->set('search.stuctrued_result', $news)->save();
-      \Drupal::service('config.factory')->getEditable('capital_news.settings')->set('search.result', $news_ra)->save();
-    }
-    return $config->get('search.result');
-  }
-
-  private function getGoogleNews(&$news, &$news_ra, $key){
-    $json = capital_common_get_rest_custom_search($key);
-    foreach($json->items as $result){
-      $news[$key][$result->title] = [
+      //$news = $config->get('search.result');
+      $results = _capital_news_getnews();
+$news=array();
+      foreach ( $results as $new){
+$news[] =  [
         '#type' => 'news_element',
-        '#label' => $result->htmlTitle,
-        '#description' => str_replace('<br>', '', $result->htmlSnippet),
-        '#url' => $result->link,
-        '#news_type' => self:: GOOGLERESULT
+        '#label' => $new['label'],
+        '#description' => $new['description'],
+        '#url' => $new['url'],
+        '#news_type' => $new['news_type'],
         ];
-      $news_ra[] = $news[$key][$result->title];
+
     }
+    return $news;
   }
 }
