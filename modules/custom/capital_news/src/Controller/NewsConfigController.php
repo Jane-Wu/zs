09e079981ba938c97fa51695804e54a8776e7b2b
Jing He <hejing.g@gmail.com>
2016-06-05 21:52:52 +0800

    Add company related news keywords list to news config

--- modules/custom/capital_news/src/Controller/NewsConfigController.php
+++ modules/custom/capital_news/src/Controller/NewsConfigController.php
@@ -13,15 +13,22 @@ use Drupal\taxonomy\Entity\Vocabulary;
 class NewsConfigController extends ControllerBase {
   public function content() {
     $vids = ['wechat_accounts', 'additional_orgs', 'hot_spots', 'oversee_spots', 'general_key_words'];
-    #$names = ["权威来源","线索来源","其他微信公众号","其他机构","热门人物","海外来风","宽泛关键词"];
+    //$names = ["权威来源","线索来源","其他微信公众号","其他机构","热门人物","海外来风","宽泛关键词"];
     $blocks = [];
     foreach( $vids as $vid_key) {
-      $block = \Drupal\block\Entity\Block::load('views_block__key_word_manage_' . $vid_key);
-      $block_content = \Drupal::entityTypeManager()
-          ->getViewBuilder('block')
-          ->view($block);
-      $blocks[] = $block_content;
+      $block_id = 'views_block__news_keywords_management_' . $vid_key;
+      $blocks[] = self::get_block_content($block_id);
     }
+    $blocks[] = self::get_block_content(
+      'views_block__company_related_news_keywords_block');
     return $blocks;
+  }
+
+  private function get_block_content($block_id) {
+    $block = \Drupal\block\Entity\Block::load($block_id);
+    $block_content = \Drupal::entityTypeManager()
+      ->getViewBuilder('block')
+      ->view($block);
+    return $block_content;
   }
 }
