6b9f4b6728f5e429c5695a0a269a1dd71f14bd92
Lang <lang.wu.jane@gmail.com>
2016-06-30 09:32:21 +0800

    Update search key

--- modules/custom/capital_news/src/Controller/NewsTestController.php
+++ modules/custom/capital_news/src/Controller/NewsTestController.php
@@ -12,7 +12,7 @@ use Drupal\Core\Controller\ControllerBase;
 class NewsTestController extends ControllerBase {
 
   public function content() {
-    _capital_news_get_wechat_news();
+    //_capital_news_get_wechat_news();
     _capital_news_get_google_news(false);
 
     $news=array();
