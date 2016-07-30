4ab647d723673e05b0d4b1a4a3d21fefe94f0db1
Lang <lang.wu.jane@gmail.com>
2016-07-30 17:41:03 +0800

    New module for RSS feeds

---
+++ modules/feeds/src/Plugin/QueueWorker/FeedRefresh.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\feeds\Plugin\QueueWorker;
+
+use Drupal\feeds\Event\FeedsEvents;
+use Drupal\feeds\Event\FetchEvent;
+use Drupal\feeds\Event\InitEvent;
+use Drupal\feeds\Exception\LockException;
+use Drupal\feeds\FeedInterface;
+use Drupal\feeds\StateInterface;
+
+/**
+ * @QueueWorker(
+ *   id = "feeds_feed_import",
+ *   title = @Translation("Feed refresh"),
+ *   cron = {"time" = 60},
+ *   deriver = "Drupal\feeds\Plugin\Derivative\FeedQueueWorker"
+ * )
+ */
+class FeedRefresh extends FeedQueueWorkerBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processItem($feed) {
+    if (!$feed instanceof FeedInterface) {
+      return;
+    }
+
+    try {
+      $feed->lock();
+    }
+    catch (LockException $e) {
+      return;
+    }
+
+    $feed->clearStates();
+
+    try {
+      $this->dispatchEvent(FeedsEvents::INIT_IMPORT, new InitEvent($feed, 'fetch'));
+      $fetch_event = $this->dispatchEvent(FeedsEvents::FETCH, new FetchEvent($feed));
+      $feed->setState(StateInterface::PARSE, NULL);
+    }
+    catch (\Exception $exception) {
+      return $this->handleException($feed, $exception);
+    }
+
+    $feed->saveStates();
+    $this->queueFactory->get('feeds_feed_parse:' . $feed->bundle())->createItem([$feed, $fetch_event->getFetcherResult()]);
+  }
+
+}
